d7b2fa459bec8ddbe4c54bc2eac78c9e09a083f7
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-15 22:57:09 +0500

    Add files via upload

---
+++ Dinner_guests.js
@@ -0,0 +1,4 @@
+"use strict";
+//Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+let Guest_List = ['NIda', 'Tooba', 'Fatima', 'Saba'];
+console.log(`I am inviting ${Guest_List.length} people to dinner.`);
