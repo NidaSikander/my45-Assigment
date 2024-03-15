ae7ffb401633cb18228b27c6940c4a9307f1cd58
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-15 22:51:13 +0500

    Add files via upload

---
+++ Greetings.ts
@@ -0,0 +1,10 @@
+//Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+
+let friendName :string[] = ["Bisma" , "Dua" , "Tooba" , "Nida"];
+
+console.log(`${friendName[0]} ,you Are My Best Friend`);
+console.log(`${friendName[1]} ,you Are My Best Friends`);
+console.log(`${friendName[2]} ,you Are My Best Friends`);
+console.log(`${friendName[3]} ,you Are My Best Friends`);
+
+
