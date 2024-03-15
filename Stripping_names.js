3cdc14a96d838d4e423a29bb6d09683b283fc547
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-15 22:46:02 +0500

    Add files via upload

---
+++ Stripping_names.js
@@ -0,0 +1,4 @@
+"use strict";
+let Name = "\t\n Nida Sikander  \t\n";
+console.log(Name);
+console.log(Name.trim());
---
+++ Stripping_names.ts
@@ -0,0 +1,5 @@
+let Name = "\t\n Nida Sikander  \t\n";
+
+console.log(Name);
+
+console.log(Name.trim());
