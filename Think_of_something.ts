741a79799229973c1c434f7730f4043c7280ce8e
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-15 22:57:48 +0500

    Add files via upload

---
+++ Think_of_something.ts
@@ -0,0 +1,6 @@
+//Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+let languages : string[] = ['english' ,'urdu' ,'hindi' ,'arabic' ,'french']
+
+console.log("Languages  I like To Learn: " ,languages);
+
