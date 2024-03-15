5a689c3d21b0694879dd2f44d2a873d89058006c
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-15 23:01:17 +0500

    Add files via upload

---
+++ International_error.js
@@ -0,0 +1,9 @@
+"use strict";
+//if you havent receieved an arry index error is one of your program yet, try to make one happen.
+//creating arry
+let newArry = ['will smith', 'james bond', 'tom cruise'];
+//assesing arry by index
+//console.log(newArry[5]);
+//make sure you correct the error before closing the program
+//correct the error
+console.log(newArry[1]);
