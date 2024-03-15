567866718b511d30623c16b0548254bc859af8ec
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-15 22:58:31 +0500

    Add files via upload

---
+++ Object.ts
@@ -0,0 +1,15 @@
+//They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these itemname :strin
+
+interface Students{
+Name:string;
+Remarks:string;
+totalMarks:number;
+}
+
+let Students={
+    Name:"Areeba",
+    Remarks:"pass",
+    totalMarks:300,
+};
+
+console.log(Students);
