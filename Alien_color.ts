0c60048f9537332f4dbebc2ea60ae81f3f9ce00a
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-15 23:04:00 +0500

    Add files via upload

---
+++ Alien_color.ts
@@ -0,0 +1,20 @@
+//Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+
+//• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+let alien_Color="green"
+
+if (alien_Color == "green"){
+
+    console.log(`the player just earned 5 points`);
+
+
+}else (alien_Color ==" white");{
+    
+};
+
+
+
+
+
+
