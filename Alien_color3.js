516dd1fb20f3e6bc3c2c0fcafe7ae0c2798a8edf
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-16 00:05:08 +0500

    Add files via upload

---
+++ Alien_color3.js
@@ -0,0 +1,21 @@
+"use strict";
+//Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-else chain.
+//• If the alien is green, print a message that the player earned 5 points.
+//• If the alien is yellow, print a message that the player earned 10 points.
+//• If the alien is red, print a message that the player earned 15 points.
+//• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+let alienColor1 = "red";
+let alienColor2 = "green";
+let alienColor3 = "yellow";
+if (alienColor1 == "red") {
+    console.log(`the player earned 5 points`);
+}
+else { }
+if (alienColor2 == "green") {
+    console.log(`the player earned 10 points`);
+}
+else { }
+if (alienColor3 == "yellow") {
+    console.log(`the player earned 15 points`);
+}
+else { }
