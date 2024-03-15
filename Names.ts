136ac00757002f2d3424f866a3d38cdec55f585d
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-15 22:50:31 +0500

    Add files via upload

---
+++ Names.ts
@@ -0,0 +1,10 @@
+//Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+let friendName = ["Bisma" , "Dua" ,"Tooba" , "Nida"];
+
+//friendName.forEach(friendname => console.log(friendname));
+
+console.log(friendName[0]);
+console.log(friendName[1]);
+console.log(friendName[2]);
+console.log(friendName[3]);
