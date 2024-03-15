c1357f4acab1769549ce286d5cb8efd451c015c6
Nida Sikander <162625937+NidaSikander@users.noreply.github.com>
2024-03-15 23:02:17 +0500

    Add files via upload

---
+++ Conditional_tests.js
@@ -0,0 +1,38 @@
+"use strict";
+//Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+//let car = 'subaru';
+//console.log("Is car == 'subaru'? I predict True.")
+//console.log(car == 'subaru')
+//• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let car = 'subaru';
+//test 1
+console.log("is car === 'sabaru'? i predict true.");
+console.log(car === 'subaru');
+//test 2
+console.log("is car == 'sabaru'? i predict true.");
+console.log(car == 'subaru');
+//TEST 3
+console.log("is car !='Honda '? i predict true.");
+console.log(car != 'Honda');
+//test 4
+console.log("is car !== 'ford')? i predict true.");
+console.log(car !== 'ford');
+//test 5
+console.log("is car.Upper case == 'SUBARU' ? i predict true.");
+console.log(car.toUpperCase() == 'SUBARU');
+//test 6
+console.log("is car.Lower Case == 'SUBARU'? i predict false.");
+console.log(car.toLowerCase() == 'SUBARU');
+//test 7
+console.log("is car === 'SUBARU'? i predict false.");
+console.log(car === 'SUBARU');
+//test 8
+console.log("is car === 'Train')? i predict false.");
+console.log(car === 'Train');
+//test 9
+console.log("is car === 'Bus')? i predict false.");
+console.log(car === 'Bus');
+//test 10
+console.log("is car === 'Cycle')? i predict false.");
+console.log(car === 'Cycle');
